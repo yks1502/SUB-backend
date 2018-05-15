4fb109aefdbe979fad60488dcc9d4c218608a778
yks1502 <516yks@naver.com>
2018-05-15 16:37:39 +0900

    remove useless

--- SUB_backend/settings.py
+++ SUB_backend/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
