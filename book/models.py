c472fc9b1233585c7af27f4b80dfc89d26e4ad43
yks1502 <516yks@naver.com>
2018-05-23 12:54:51 +0900

    implement book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+  itemId = models.IntegerField(primary_key=True)
+  title = models.CharField(max_length=100)
+  publisher = models.CharField(max_length=100)
+  author = models.CharField(max_length=100)
+  priceStandard = models.IntegerField()
+  image = models.ImageField()
