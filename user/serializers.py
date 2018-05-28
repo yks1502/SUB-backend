6d96658e80237d667cf45e276834639c8325a913
yks1502 <516yks@naver.com>
2018-05-28 19:30:03 +0900

    shows my sales and purchases on profile

--- user/serializers.py
+++ user/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 
 from user.models import User
+from transaction.models import Sale, Purchase
 
 class UserSerializer(serializers.ModelSerializer):
+  sale_owner = serializers.PrimaryKeyRelatedField(many=True, queryset=Sale.objects.all())
+  purchase_owner = serializers.PrimaryKeyRelatedField(many=True, queryset=Purchase.objects.all())
   class Meta:
     model = User
-    fields = ('id', 'username', 'email', 'nickname', 'isConfirmed', 'created', 'updated', 'confirmationToken')
+    fields = ('id', 'username', 'email', 'nickname', 'isConfirmed', 'created', 'updated', 'confirmationToken',
+    'sale_owner', 'purchase_owner')
 
 class NicknameSerializer(serializers.ModelSerializer):
   class Meta:
