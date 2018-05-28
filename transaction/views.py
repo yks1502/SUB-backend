e17cd7f798a30c633daeb7040d3b45fe77b04ce3
yks1502 <516yks@naver.com>
2018-05-28 17:37:37 +0900

    get nickname of the owner and partly implement comment model

--- transaction/views.py
+++ transaction/views.py
@@ -2,23 +2,31 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 
-from transaction.models import Sale, Purchase
-from transaction.serializers import SaleSerializer, PurchaseSerializer
+from transaction.models import Sale, Purchase, SaleComment, PurchaseComment
+from transaction.serializers import *
 from user.models import User
 from user.permissions import IsOwner, IsOwnerOrReadOnly
 
 class SaleList(generics.ListCreateAPIView):
   queryset = Sale.objects.all()
-  serializer_class = SaleSerializer
   permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+  def get_serializer_class(self):
+    if self.request.method == 'GET':
+      return SaleRetrieveSerializer
+    return SaleCreateSerializer
 
   def perform_create(self, serializer):
     serializer.save(userId=self.request.user)
 
 class SaleDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Sale.objects.all()
-  serializer_class = SaleSerializer
   permission_classes = (IsOwnerOrReadOnly,)
+
+  def get_serializer_class(self):
+    if self.request.method == 'GET':
+      return SaleRetrieveSerializer
+    return SaleCreateSerializer
 
 @api_view(['POST'])
 @permission_classes((IsOwner,))
@@ -39,23 +47,31 @@ def complete_sale(request, pk):
 
 class PurchaseList(generics.ListCreateAPIView):
   queryset = Purchase.objects.all()
-  serializer_class = PurchaseSerializer
   permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+  def get_serializer_class(self):
+    if self.request.method == 'GET':
+      return PurchaseRetrieveSerializer
+    return PurchaseCreateSerializer
 
   def perform_create(self, serializer):
     serializer.save(userId=self.request.user)
 
 class PurchaseDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Purchase.objects.all()
-  serializer_class = PurchaseSerializer
   permission_classes = (IsOwnerOrReadOnly,)
 
+  def get_serializer_class(self):
+    if self.request.method == 'GET':
+      print(self.request.data)
+      return PurchaseRetrieveSerializer
+    return PurchaseCreateSerializer
 
 @api_view(['POST'])
 @permission_classes((IsOwner,))
 def complete_purchase(request, pk):
   user = request.user
-  purchase = Purchase.object.get(pk = pk)
+  purchase = Purchase.object.get(pk=pk)
   if user.id is None or user.id != purchase.userId:
     return Response(
       data = {'message': 'not authorized'},
@@ -67,3 +83,7 @@ def complete_purchase(request, pk):
     data = {'message': '거래가 완료되었습니다.'},
     status = status.HTTP_200_OK,
   )
+
+class SaleCommentList(generics.ListCreateAPIView):
+  queryset = SaleComment.objects.all()
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
