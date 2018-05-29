4fc8f8bbb34b54fe392a486ada46d9ea4229a360
yks1502 <516yks@naver.com>
2018-05-29 18:58:43 +0900

    fix icontains

--- user/views.py
+++ user/views.py
@@ -95,7 +95,7 @@ def confirm_email(request):
 @api_view(['POST'])
 def duplicate_username(request):
   username = request.data.get('username', None)
-  if User.objects.all().filter(username__icontains=username):
+  if User.objects.all().filter(username=username):
     return Response(
       data = {'message': '중복되는 아이디가 존재합니다'},
       status = status.HTTP_403_FORBIDDEN,
