6fb4b91ffb4e301705c04df101c84b877b0a8a40
yks1502 <516yks@naver.com>
2018-06-04 11:38:00 +0900

    update book title, author, publisher max length

--- book/models.py
+++ book/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Book(models.Model):
   itemId = models.IntegerField(primary_key=True)
-  title = models.CharField(max_length=100)
+  title = models.CharField(max_length=200)
   publisher = models.CharField(max_length=100)
-  author = models.CharField(max_length=100)
+  author = models.CharField(max_length=1000)
   priceStandard = models.IntegerField()
   image = models.ImageField()
